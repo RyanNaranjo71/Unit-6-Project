6c5e5adf6e90747f15b4fe19d3e833ccb481c17e
RyanNaranjo71 <95878856+RyanNaranjo71@users.noreply.github.com>
2022-06-14 13:11:50 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+$(".pass").click(function() {
+    $(".slideone").hide();
+    $(".slidetwo").show();
+});
+
+$(".next2").click(function() {
+    $(".slidetwo").hide();
+    $(".slidethree").show();
+});
+
+$(".next3").click(function() {
+    $(".slidethree").hide();
+    $(".slidefour").show();
+});
+
+$(".next4").click(function() {
+    $(".slidefour").hide();
+    $(".slidefive").show();
+});
+
+$(".next5").click(function() {
+    $(".slidefive").hide();
+    $(".slidesix").show();
+});
